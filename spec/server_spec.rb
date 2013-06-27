10057a01b18a59eff74b127b861a04612a2244df
Ionuț Arțăriși <iartarisi@suse.cz>
2013-06-27 14:02:02 +0200

    remove unneeded node attributes in tests
    Change-Id: Ifd75506f8989072f96edef4b43e7587ee413d177

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -2,18 +2,12 @@ require_relative 'spec_helper'
 
 describe 'openstack-network::server' do
 
-  #-------------------
-  # UBUNTU
-  #-------------------
-
   describe "ubuntu" do
 
     before do
       quantum_stubs
       @chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS
       @node = @chef_run.node
-      @node.set['lsb']['code'] = 'precise'
-      @node.set['openstack']['developer_mode'] = true
 
       # mock out an interface on the storage node
       @node.set["network"] = MOCK_NODE_NETWORK_DATA['network']
@@ -21,7 +15,7 @@ describe 'openstack-network::server' do
       @chef_run.converge "openstack-network::server"
     end
 
-    it "installs quamtum packages" do
+    it "installs quantum packages" do
       expect(@chef_run).to install_package "quantum-server"
     end
 
