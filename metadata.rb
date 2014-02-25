2543667cb01ab9d55e9a23139e8f241d0fbc41d2
gengjh <gengjh@cn.ibm.com>
2014-02-25 10:54:15 +0800

    Update the neutron.conf.erb according to the latest ver from upstream.
    - Move the database section into neutron.conf from plugins
    - Make the service_provider attribute configurable
    
    Implements: blueprint neutron-ml2
    Change-Id: Ia9e932a490e5c13ba49aa8358356e2fa640b845c

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer        'Jay Pipes <jaypipes@gmail.com>'
 license           'Apache 2.0'
 description       'Installs and configures the OpenStack Network API Service and various agents and plugins'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '8.1.1'
+version           '8.2.0'
 recipe            'openstack-network::client', 'Install packages required for network client'
 recipe            'openstack-network::server', 'Installs packages required for a OpenStack Network server'
 recipe            'openstack-network::openvswitch', 'Installs packages required for OVS'
@@ -17,6 +17,6 @@ recipe            'openstack-network::identity_registration', 'Registers OpenSta
 end
 
 depends           'openstack-identity', '~> 8.0'
-depends           'openstack-common', '~> 8.0'
+depends           'openstack-common', '~> 8.2'
 depends           'mysql'
 depends           'postgresql'
