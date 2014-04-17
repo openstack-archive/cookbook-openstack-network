853fa9fcc6d7e9e6607be188299bf6c9208f24f7
Mark Vanderwiel <vanderwl@us.ibm.com>
2014-04-17 11:26:11 -0500

    Remove policy json file
    Remove the policy.json template, as they contain no templated variables.
    This would allow use of the policy.json files provided via the package,
    and decrease the need to sync them with upstream
    
    Change-Id: Iad2562d54907d8987c93c2ee41147a1027f57a69
    Implements: blueprint remove-policy-templates

--- recipes/common.rb
+++ recipes/common.rb
@@ -90,15 +90,6 @@ template '/etc/neutron/rootwrap.conf' do
   mode 00644
 end
 
-template '/etc/neutron/policy.json' do
-  source 'policy.json.erb'
-  owner node['openstack']['network']['platform']['user']
-  group node['openstack']['network']['platform']['group']
-  mode 00644
-
-  notifies :restart, 'service[neutron-server]', :delayed
-end
-
 mq_service_type = node['openstack']['mq']['network']['service_type']
 
 if mq_service_type == 'rabbitmq'
