5d727d62792e462765f45d2105783c2065581c66
Mathew Odden <mrodden@linux.vnet.ibm.com>
2014-05-19 21:24:13 +0000

    Revert "switch to using auth_url instead of auth_host et al"
    This reverts commit 6d252ac4841d66eb3ced83bdce09db0e16c7f9b2.
    
    keystone_authtoken middleware has no config option for auth_url,
    so the change breaks neutron-server from being able to contact the
    correct keystone service (unless it runs on localhost,
    which is what auth_host defaults to)
    
    Related-Bug: #1319280
    Change-Id: I41a2ecd0ada3ec59983fd408123b0f3570050bb1

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -293,9 +293,24 @@ describe 'openstack-network::server' do
           'admin_password = neutron-pass')
       end
 
-      it 'has auth_url' do
+      it 'has auth_uri' do
         expect(chef_run).to render_file(file.name).with_content(
-          'auth_url = http://127.0.0.1:5000/v2.0')
+          'auth_uri = http://127.0.0.1:5000/v2.0')
+      end
+
+      it 'has auth_host' do
+        expect(chef_run).to render_file(file.name).with_content(
+          'auth_host = 127.0.0.1')
+      end
+
+      it 'has auth_port' do
+        expect(chef_run).to render_file(file.name).with_content(
+          'auth_port = 35357')
+      end
+
+      it 'has auth_protocol' do
+        expect(chef_run).to render_file(file.name).with_content(
+          'auth_protocol = http')
       end
 
       it 'has signing_dir' do
