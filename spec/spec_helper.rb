30bd0d08a1593d2aa0f39003e51bc7e221780f46
Mark Vanderwiel <vanderwl@us.ibm.com>
2014-07-18 12:59:40 -0500

    Fix fauxhai platform versions for suse and redhat
    Change-Id: If9b8802d99e78acbb7cecf2ce44f5afa85191abb
    Partial-Bug: #1344199

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,12 @@ require 'chef/application'
 LOG_LEVEL = :fatal
 SUSE_OPTS = {
   platform: 'suse',
-  version: '11.03',
+  version: '11.3',
   log_level: LOG_LEVEL
 }
 REDHAT_OPTS = {
     platform: 'redhat',
-    version: '6.3',
+    version: '6.5',
     log_level: LOG_LEVEL
 }
 UBUNTU_OPTS = {
