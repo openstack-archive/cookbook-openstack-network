3868368ecace965097b59ee85fb3909f473955ee
Jan Klare <j.klare@cloudbau.de>
2017-08-17 14:19:33 +0200

    starting pike development patch
    Change-Id: I54d6b08011eae2d7a47a6b56214c5170fd7e71b3

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license          'Apache 2.0'
 description      'Installs and configures the OpenStack Network API Service and various agents and plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '15.0.0'
+version          '16.0.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
-depends 'openstack-common', '>= 15.0.0'
-depends 'openstack-identity', '>= 15.0.0'
+depends 'openstack-common', '>= 16.0.0'
+depends 'openstack-identity', '>= 16.0.0'
 depends 'openstackclient'
 
 issues_url 'https://launchpad.net/openstack-chef' if respond_to?(:issues_url)
