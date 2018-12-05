66490ada348089b666789349c4d42b86f22970dc
ZhijunWei <wzj334965317@outlook.com>
2018-12-04 23:21:04 -0500

    Change openstack-dev to openstack-discuss
    Change-Id: I9596e05c8c9ff069087b17ed858960a4e608d23d

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'openstack-network'
 maintainer       'openstack-chef'
-maintainer_email 'openstack-dev@lists.openstack.org'
+maintainer_email 'openstack-discuss@lists.openstack.org'
 license          'Apache-2.0'
 description      'Installs and configures the OpenStack Network API Service and various agents and plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
