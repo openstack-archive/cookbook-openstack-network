70fda6ae3a87fa962214daff6e8bc2ec39afb836
Lance Albertson <lance@osuosl.org>
2019-12-05 18:18:06 -0800

    Updates for rocky
    - Replace git.openstack.org with opendev.org
    - Update some documentation
    - Move README.md to README.rst for better rendering
    - Drop obsolete bootstrap.sh script
    
    Change-Id: Ia621986157956cb8f03fabd00741959bd2879aff

--- Berksfile
+++ Berksfile
@@ -1,14 +1,11 @@
 source 'https://supermarket.chef.io'
 
-%w(common identity).each do |cookbook|
-  if Dir.exist?("../cookbook-openstack-#{cookbook}")
-    cookbook "openstack-#{cookbook}", path: "../cookbook-openstack-#{cookbook}"
+%w(-common -identity client).each do |cookbook|
+  if Dir.exist?("../cookbook-openstack#{cookbook}")
+    cookbook "openstack#{cookbook}", path: "../cookbook-openstack#{cookbook}"
   else
-    cookbook "openstack-#{cookbook}", git: "https://opendev.org/openstack/cookbook-openstack-#{cookbook}"
+    cookbook "openstack#{cookbook}", git: "https://opendev.org/openstack/cookbook-openstack#{cookbook}"
   end
 end
-
-cookbook 'openstackclient',
-  git: 'https://opendev.org/openstack/cookbook-openstackclient'
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -3,17 +3,33 @@ maintainer       'openstack-chef'
 maintainer_email 'openstack-discuss@lists.openstack.org'
 license          'Apache-2.0'
 description      'Installs and configures the OpenStack Network API Service and various agents and plugins'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '18.0.0'
+
+recipe 'openstack-network::_bridge_config_example', 'Example bridge recipe used in kitchen tests'
+recipe 'openstack-network::db_migration', 'Migrates the neutron database'
+recipe 'openstack-network::default', 'Configures common pieces needed for all neutron services and create the neutron.conf'
+recipe 'openstack-network::dhcp_agent', 'Installs the DHCP agent'
+recipe 'openstack-network::fwaas', 'Installs the Firewall as a Service'
+recipe 'openstack-network::identity_registration', 'Registers the OpenStack Network API endpoint and service user with Keystone'
+recipe 'openstack-network::l3_agent', 'Installs the L3 agent'
+recipe 'openstack-network::lbaas', 'Installs the Loadbalancer as a Service'
+recipe 'openstack-network::metadata_agent', 'Installs the metadata agent'
+recipe 'openstack-network::metering_agent', 'Installs the metering agent'
+recipe 'openstack-network::ml2_core_plugin', 'Configure the ml2_core_plugin'
+recipe 'openstack-network::ml2_linuxbridge', 'Configure the ml2 linuxbridge plugin'
+recipe 'openstack-network::ml2_openvswitch', 'Configure the ml2 openvswitch plugin'
+recipe 'openstack-network::openvswitch', 'Installs openvswitch'
+recipe 'openstack-network::openvswitch_agent', 'Installs the openvswitch agent'
+recipe 'openstack-network::plugin_config', 'Generates all the needed plugin configurations directly from the attributes'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
+depends 'openstackclient'
 depends 'openstack-common', '>= 18.0.0'
 depends 'openstack-identity', '>= 18.0.0'
-depends 'openstackclient'
 
-issues_url 'https://launchpad.net/openstack-chef' if respond_to?(:issues_url)
-source_url 'https://github.com/openstack/cookbook-openstack-network' if respond_to?(:source_url)
-chef_version '>= 12.5' if respond_to?(:chef_version)
+issues_url 'https://launchpad.net/openstack-chef'
+source_url 'https://opendev.org/openstack/cookbook-openstack-network'
+chef_version '>= 14.0'
